da756673257014ab74e636a5652f68b6facf4f62
Sutha232 <sutha.c@ku.th>
2021-01-10 14:34:07 +0700

    add name

---
+++ comment/variable/name.py
@@ -0,0 +1,14 @@
+#สามารถประกาศตัวแปรได้
+myver = "Oat"
+my_var = "Oat"
+_My_var = "Oat"
+myVar = "Oat"
+MYVAR = "Oat"
+myvar2 = "Oat"
+
+#ไม่สามารถประกาศตัวแปรได้
+2Myvar = "Oat"
+my-var = "Oat"
+my var = "Oat"
+
+
